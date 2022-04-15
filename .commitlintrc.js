5b65af367bef87d908a7579901ce949e89657c12
Aiden <31586885+Aiden-FE@users.noreply.github.com>
2022-04-15 18:15:33 +0800

    test: 测试校验

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,9 +1,7 @@
 'use strict';
 
 module.exports = {
-    extends: ["cz"],
-    rules: {
-        'type-empty': [2, 'never'],
-        'subject-empty': [2, 'never']
-    }
+    extends: [
+        "@commitlint/config-conventional"
+    ],
 };
