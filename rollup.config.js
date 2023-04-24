3ab67795547e02a5bc447b538d76fadfdf7c6c56
Aiden-FE <Aiden_FE@outlook.com>
2023-04-24 11:40:55 +0800

    fix: cjs output add exports

--- rollup.config.js
+++ rollup.config.js
@@ -104,6 +104,7 @@ export default [
     output: getOutput({
       format: 'cjs',
       entryFileNames: pkg.commonjs.replace('dist/', ''),
+      exports: 'auto',
     }),
     external: getExternal(),
     plugins: getPlugins(undefined, {
